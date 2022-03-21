4e9a464339a6ac7b65a328c3a316488c89d72841
Arne_van_der_Lei <arne@vanderlei.be>
2022-03-21 13:55:36 +0100

    added torch resepie

--- TownCrafter/Recepies.lua
+++ TownCrafter/Recepies.lua
@@ -3,6 +3,15 @@ return {
         layout = {"c  ","cc ","ccc"},
         blocks = {
             ["c"]="minecraft:cobblestone"
-        }
+        },
+        output = 4
+    },
+    ["minecraft:torch"] = {
+        layout={"c  ","s  ","   "},
+        blocks={
+            ["c"] = "minecraft:coal",
+            ["s"] = "minecraft:stick"
+        },
+        output = 4
     }
 }
