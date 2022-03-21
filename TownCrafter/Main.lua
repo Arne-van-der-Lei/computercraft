60a18ca72fff2e7eeb3993516001a8df7a5a3abc
Arne_van_der_Lei <arne@vanderlei.be>
2022-03-21 15:44:00 +0100

    changes

--- TownCrafter/Main.lua
+++ TownCrafter/Main.lua
@@ -38,6 +38,7 @@ function CraftItem(item,amount,right)
         for i = 1, 3, 1 do
             for j = 1, #recepie.layout[i] do
                 local c = recepie.layout[i]:sub(j,j)
+                print(c)
                 item = recepie.blocks[c]
                 if GetItemFromChest(item,(i-1)*4+j,1) == false then
                     ClearInventory()
