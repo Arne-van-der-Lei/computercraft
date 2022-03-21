e2e1b0488a6117a8ce65dd5e80ba7f4ed329d0a9
Arne_van_der_Lei <arne@vanderlei.be>
2022-03-21 11:51:10 +0100

    added startup

---
+++ Startup.lua
@@ -0,0 +1,15 @@
+label = os.getComputerLabel()
+
+function download(url, file)
+    local content = http.get("https://raw.githubusercontent.com/Arne-van-der-Lei/computercraft/master/" .. url).readAll()
+    if not content then
+      error("Could not connect to website")
+    end
+    f = fs.open(file, "w")
+    f.write(content)
+    f.close()
+  end
+
+download(label.."/main.lua","main.lua")
+
+shell.run("main.lua")
