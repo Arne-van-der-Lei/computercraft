ac3b9bf1558e6630bdae1a46204501819715dae7
Arne_van_der_Lei <arne@vanderlei.be>
2022-03-21 17:40:59 +0100

    added more recepies

--- TownCrafter/Recepies.lua
+++ TownCrafter/Recepies.lua
@@ -13,10 +13,24 @@ return {
         },
         output = 4
     },
+    ["minecraft:spruce_planks"] = {
+        layout = {"w  ","   ","   "},
+        blocks = {
+            ["w"]="minecraft:spruce_log"
+        },
+        output = 4
+    },
     ["minecraft:oak_stairs"] = {
         layout = {"w  ","ww ","www"},
         blocks = {
             ["w"]="minecraft:oak_planks"
+        },
+        output = 4
+    },
+    ["minecraft:spruce_stairs"] = {
+        layout = {"w  ","ww ","www"},
+        blocks = {
+            ["w"]="minecraft:spruce_planks"
         },
         output = 4
     },
