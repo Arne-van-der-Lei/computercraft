d3a102a95fc23289aae6722c4920d95617e63c14
Arne_van_der_Lei <arne@vanderlei.be>
2022-03-21 15:50:14 +0100

    changes to maybe fix it

--- TownCrafter/Recepies.lua
+++ TownCrafter/Recepies.lua
@@ -2,15 +2,15 @@ return {
     ["minecraft:cobblestone_stairs"] = {
         layout = {"c  ","cc ","ccc"},
         blocks = {
-            ['c']="minecraft:cobblestone"
+            ["c"]="minecraft:cobblestone"
         },
         output = 4
     },
     ["minecraft:torch"] = {
         layout={"c  ","s  ","   "},
         blocks={
-            ['c'] = "minecraft:coal",
-            ['s'] = "minecraft:stick"
+            ["c"] = "minecraft:coal",
+            ["s"] = "minecraft:stick"
         },
         output = 4
     }
